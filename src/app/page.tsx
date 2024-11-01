c7c95fc5e6a4f50a43ba63492b1fdf91b5371267
myMac <ghaliwali@gmail.com>
2024-11-01 14:44:35 +0100

    working on the 3d component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
-import ModelViewer from '@/components/ModelViewer'
+import Bar from '@/components/Bar'
 
 export default function Home() {
   return (
     <main className="min-h-screen flex flex-col items-center justify-center p-8">
       <h1 className="mb-8">Hello world I&apos;m here to welcome you to my new app</h1>
-      <ModelViewer />
+      {/* <Bar /> */}
     </main>
   )
 }
