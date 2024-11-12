641773307e9c7e6d9423d73fb9957c8378c8be57
myMac <ghaliwali@gmail.com>
2024-11-12 13:55:51 +0100

    deploy please

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,7 @@
-'use client'
-
-import dynamic from 'next/dynamic'
-
-const Bar = dynamic(() => import('../components/Bar'), { 
-  ssr: false,
-  loading: () => <div>Loading...</div>
-})
-
-export default function Page() {
+export default function Home() {
   return (
-    <div>
-      <Bar />
-    </div>
+    <main className="min-h-screen flex flex-col items-center justify-center p-8">
+      <h1 className="mb-8">Hello world I&apos;m here to welcome you to my new app</h1>
+    </main>
   )
 }
-
-// import Bar from '@/components/Bar'
-
-// export default function Home() {
-//   return (
-//     <main className="min-h-screen flex flex-col items-center justify-center p-8">
-//       <h1 className="mb-8">Hello world I&apos;m here to welcome you to my new app</h1>
-//       <Bar />
-//     </main>
-//   )
-// }
